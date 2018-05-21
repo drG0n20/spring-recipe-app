5db1bc77a97a0a44421f04679e456622b6e653b2
Br0n50n <marcinster@gmail.com>
2018-05-21 19:08:02 +0200

    Initial commit

---
+++ src/main/java/com/drg0n20/springrecipeapp/SpringRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package com.drg0n20.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/drg0n20/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drg0n20.springrecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
