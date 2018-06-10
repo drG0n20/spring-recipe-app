ebd1fca284ebe97ba9d6ccbf09916993e1737198
Br0n50n <marcinster@gmail.com>
2018-06-10 13:16:41 +0200

    Add Unit and Integration Tests

---
+++ src/test/java/com/drg0n20/springrecipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.drg0n20.springrecipeapp.services;
+
+import com.drg0n20.springrecipeapp.domain.Recipe;
+import com.drg0n20.springrecipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
